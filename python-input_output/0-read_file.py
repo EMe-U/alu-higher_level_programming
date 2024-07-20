d65a0408613f208b8dfb348b5211da9b749ac21e
EMe-U <e.umurerwa@alustudent.com>
2024-07-20 09:10:44 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,3 +2,7 @@
 read_file = __import__('0-read_file').read_file
 
 read_file("my_file_0.txt")
+def read_file(filename=""):
+    with open(filename, "r", encoding="utf-8") as file:
+        print(file.read())
+
