c8b0693fd9cecc0aaff9c9f81f6353b82b2bc0ff
EMe-U <e.umurerwa@alustudent.com>
2024-07-20 09:46:30 -0700

    write file

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,8 +1,8 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 '''script to write a string to a text file(UTF8) to return character num'''
 
 
 def write_file(filename="", text=""):
-  """Writes a string to a text file (UTF8) and returns the number of characters written"""
-    with open(filename, "w", encoding="utf-8") as file:
-        return file.write(text)
+    '''writes string to text file and returns characters number'''
+    with open(filename, 'w', encoding="utf-8") as f:
+        return f.write(text)
