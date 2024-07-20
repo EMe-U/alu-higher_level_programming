387f41c5504c108c1a29f400ae7dfd642266188a
EMe-U <e.umurerwa@alustudent.com>
2024-07-20 09:17:03 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -3,14 +3,6 @@ read_file = __import__('0-read_file').read_file
 
 read_file("my_file_0.txt")
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints it to stdout.
-
-    Args:
-        filename (str): The name of the file to read. Defaults to an empty string.
-
-    Returns:
-        None
-    """
+  '''reads the data from outside file'''
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
