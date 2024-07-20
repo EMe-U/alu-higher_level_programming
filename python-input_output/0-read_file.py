8660bba17db63566ce14249e11da5b54bc59d210
EMe-U <e.umurerwa@alustudent.com>
2024-07-20 09:19:01 -0700

    Read file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-read_file = __import__('0-read_file').read_file
+'''function that reads a file UTF8'''
 
-read_file("my_file_0.txt")
+
 def read_file(filename=""):
-  '''reads the data from outside file'''
-    with open(filename, "r", encoding="utf-8") as file:
-        print(file.read())
+    '''reads the data from outside file'''
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end="")
