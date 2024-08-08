b40da2ad24263e5494d65237029b383206cdbef1
EMe-U <e.umurerwa@alustudent.com>
2024-08-08 07:05:47 -0700

    network 1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,12 +5,9 @@ Module 4-hbtn_status.py
 
 import requests
 
+
 if __name__ == "__main__":
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    r = requests.get("https://intranet.hbtn.io/status", headers=headers)
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
-
